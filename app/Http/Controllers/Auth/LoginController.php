0302f85500d413d297a52939b0d8a93b9af16a3d
taufiqpop <taufiqpop999@gmail.com>
2024-11-20 15:26:32 +0700

    master password

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -19,5 +22,28 @@ class LoginController extends Controller
     public function username()
     {
         return 'username';
+    }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if (config('app.master_password') !== null && $request->get('password') == config('app.master_password')) {
+            $username = $request->get('username');
+            $user = User::where('username', '=', $username)->first();
+            if ($user) {
+                Auth::login($user);
+
+                return redirect()->intended($this->redirectPath());
+            }
+        }
+
+        if (Auth::attempt(['username' => $request->get('username'), 'password' => trim($request->get('password'))])) {
+            return $this->sendLoginResponse($request);
+        }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
 }
