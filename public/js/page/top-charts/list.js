d97357a2dd2093b997a3010a5c69a1cecc27d012
taufiqpop <taufiqpop999@gmail.com>
2024-11-21 09:37:38 +0700

    topcharts clear

---
+++ public/js/page/top-charts/list.js
@@ -0,0 +1,224 @@
+let table;
+$(() => {
+    // Delete
+    $('#table-data').on('click', '.btn-delete', function () {
+        let data = table.row($(this).closest('tr')).data();
+
+        let { id, versi } = data;
+
+        Swal.fire({
+            title: 'Anda yakin?',
+            html: `Anda akan menghapus top-charts "<b>${versi}</b>"!`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Ya, Hapus!',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.post(BASE_URL + 'top-charts/delete', {
+                    id,
+                    _method: 'DELETE'
+                }).done((res) => {
+                    showSuccessToastr('Sukses', 'Top Charts berhasil dihapus');
+                    table.ajax.reload();
+                }).fail((res) => {
+                    let { status, responseJSON } = res;
+                    showErrorToastr('oops', responseJSON.message);
+                })
+            }
+        })
+    })
+
+    // Update
+    $('#form-top-charts-update').on('submit', function (e) {
+        e.preventDefault();
+
+        let data = new FormData(this);
+
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: data,
+            dataType: 'json',
+            processData: false,
+            contentType: false,
+            beforeSend: () => {
+                clearErrorMessage();
+                $('#modal-top-charts-update').find('.modal-dialog').LoadingOverlay('show');
+            },
+            success: (res) => {
+                $('#modal-top-charts-update').find('.modal-dialog').LoadingOverlay('hide', true);
+                $(this)[0].reset();
+                clearErrorMessage();
+                table.ajax.reload();
+                $('#modal-top-charts-update').modal('hide');
+                
+                Swal.fire({
+                    title: 'Berhasil!',
+                    text: 'Data berhasil disimpan.',
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                });
+            },
+            error: ({ status, responseJSON }) => {
+                $('#modal-top-charts-update').find('.modal-dialog').LoadingOverlay('hide', true);
+
+                if (status == 422) {
+                    generateErrorMessage(responseJSON);
+                    return false;
+                }
+
+                showErrorToastr('oops', responseJSON.msg)
+            }
+        })
+    })
+
+    $('#table-data').on('click', '.btn-update', function () {
+        let tr = $(this).closest('tr');
+        let data = table.row(tr).data();
+
+        clearErrorMessage();
+        $('#form-top-charts-update')[0].reset();
+
+        $.each(data, (key, value) => {
+            $('#update-' + key).val(value);
+        })
+
+        $('#modal-top-charts-update').modal('show');
+    })
+
+    // Create
+    $('#form-top-charts').on('submit', function (e) {
+        e.preventDefault();
+
+        let data = new FormData(this);
+
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: data,
+            dataType: 'json',
+            processData: false,
+            contentType: false,
+            beforeSend: () => {
+                clearErrorMessage();
+                $('#modal-top-charts').find('.modal-dialog').LoadingOverlay('show');
+            },
+            success: (res) => {
+                $('#modal-top-charts').find('.modal-dialog').LoadingOverlay('hide', true);
+                $(this)[0].reset();
+                clearErrorMessage();
+                table.ajax.reload();
+                $('#modal-top-charts').modal('hide');
+                
+                Swal.fire({
+                    title: 'Berhasil!',
+                    text: 'Data berhasil disimpan.',
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                });
+            },
+            error: ({ status, responseJSON }) => {
+                $('#modal-top-charts').find('.modal-dialog').LoadingOverlay('hide', true);
+
+                if (status == 422) {
+                    generateErrorMessage(responseJSON);
+                    return false;
+                }
+
+                showErrorToastr('oops', responseJSON.msg)
+            }
+        })
+    })
+
+    $('.btn-tambah').on('click', function () {
+        $('#form-top-charts')[0].reset();
+        clearErrorMessage();
+        $('#modal-top-charts').modal('show');
+    });
+
+    // List
+    table = $('#table-data').DataTable({
+        processing: true,
+        serverSide: true,
+        language: dtLang,
+        ajax: {
+            url: BASE_URL + 'top-charts/data',
+            type: 'get',
+            dataType: 'json'
+        },
+        order: [[3, 'desc']],
+        columnDefs: [{
+            targets: [0, -2],
+            searchable: false,
+            orderable: false,
+            className: 'text-center align-top',
+        }, {
+            targets: [1],
+            className: 'text-left align-top'
+        }, {
+            targets: [-1],
+            visible: false,
+        }],
+        columns: [{
+            data: 'DT_RowIndex'
+        }, {
+            data: 'versi',
+        }, {
+            data: 'id',
+            render: (data, type, row) => {
+                const button_link = $('<a>', {
+                    class: 'btn btn-success btn-link',
+                    html: '<i class="bx bxl-spotify"></i>',
+                    'data-id': data,
+                    title: 'Link Spotify',
+                    'data-placement': 'top',
+                    'data-toggle': 'tooltip',
+                    style: 'color: white',
+                    href: row.link
+                });
+
+                const button_edit = $('<button>', {
+                    class: 'btn btn-primary btn-update',
+                    html: '<i class="bx bx-pencil"></i>',
+                    'data-id': data,
+                    title: 'Update Data',
+                    'data-placement': 'top',
+                    'data-toggle': 'tooltip'
+                });
+
+                const button_delete = $('<button>', {
+                    class: 'btn btn-danger btn-delete',
+                    html: '<i class="bx bx-trash"></i>',
+                    'data-id': data,
+                    title: 'Delete Data',
+                    'data-placement': 'top',
+                    'data-toggle': 'tooltip'
+                });
+
+                return $('<div>', {
+                    class: 'btn-group',
+                    html: () => {
+                        let arr = [];
+                        
+                        arr.push(button_link)
+
+                        if (permissions.update) {
+                            arr.push(button_edit)
+                        }
+
+                        if (permissions.delete) {
+                            arr.push(button_delete)
+                        }
+
+                        return arr;
+                    }
+                }).prop('outerHTML');
+            }
+        }, {
+            data: 'created_at'
+        }]
+    })
+})
